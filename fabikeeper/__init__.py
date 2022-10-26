7dd8a6c62dcbe9f9d92c3448199a826300b240c4
fabichoi <fabianus.c@gmail.com>
2022-10-26 21:27:15 +0900

    [API] router 예제 실습

--- fabikeeper/__init__.py
+++ fabikeeper/__init__.py
@@ -11,4 +11,32 @@ def create_app():
     def index():
         return 'welcome to the fabikeeper __init__'
 
+    ''' Routing '''
+    from flask import jsonify, redirect, url_for
+    from markupsafe import escape
+
+    @app.route('/test/name/<name>')
+    def name(name):
+        return f'Name is {name}, {escape({type(name)})}'
+
+    @app.route('/test/id/<int:id>')
+    def id(id):
+        return 'ID: %d' % id
+
+    @app.route('/test/path/<path:subpath>')
+    def path(subpath):
+        return subpath
+
+    @app.route('/test/json')
+    def json():
+        return jsonify({'hello': 'world'})
+
+    @app.route('/test/redirect/<path:subpath>')
+    def redirect_url(subpath):
+        return redirect(subpath)
+
+    @app.route('/test/urlfor/<path:subpath>')
+    def urlfor(subpath):
+        return redirect(url_for('path', subpath=subpath))
+
     return app
