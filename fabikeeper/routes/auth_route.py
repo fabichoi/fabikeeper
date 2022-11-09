e49afad6c0e0773aeafb833bddfc43fb39e0cdd7
fabichoi <fabianus.c@gmail.com>
2022-11-09 23:10:14 +0900

    [API] 세션기반 로그인/로그아웃/회원가입 구현(메모리)

--- fabikeeper/routes/auth_route.py
+++ fabikeeper/routes/auth_route.py
@@ -1,8 +1,24 @@
-from flask import Blueprint, render_template, redirect, url_for, flash
+from flask import Blueprint, render_template, redirect, url_for, flash, session, request
 from fabikeeper.forms.auth_form import LoginForm, RegisterForm
 
 NAME = 'auth'
 bp = Blueprint(NAME, __name__, url_prefix='/auth')
+
+from dataclasses import dataclass
+
+USERS = []
+
+
+@dataclass
+class User:
+    user_id: str
+    user_name: str
+    password: str
+
+
+USERS.append(User('abcd', 'abcd', '1234'))
+USERS.append(User('admin', 'admin', '1234'))
+USERS.append(User('tester', 'tester', '1234'))
 
 
 @bp.route('/')
@@ -16,7 +32,16 @@ def login():
     if form.validate_on_submit():
         user_id = form.data.get('user_id')
         password = form.data.get('password')
-        return f'{user_id}, {password}'
+        user = [user for user in USERS if user.user_id == user_id]
+        if user:
+            user = user[0]
+            if user.password != password:
+                flash('Password is not valid.')
+            else:
+                session['user_id'] = user_id
+                return redirect(url_for('base.index'))
+        else:
+            flash('User ID is not exists.')
     else:
         flash_form_errors(form)
     return render_template(f'{NAME}/login.html', form=form)
@@ -31,7 +56,21 @@ def register():
         user_name = form.data.get('user_name')
         password = form.data.get('password')
         repassword = form.data.get('repassword')
-        return f'{user_id}, {password}'
+        user = [user for user in USERS if user.user_id == user_id]
+        if user:
+            flash('User ID is already exists.')
+            return redirect(request.path)
+        else:
+            USERS.append(
+                User(
+                    user_id=user_id,
+                    user_name=user_name,
+                    password=password
+                )
+            )
+            session['user_id'] = user_id
+            return redirect(url_for('base.index'))
+        return f'{user_id}, {user_name}, {password}, {repassword}'
     else:
         flash_form_errors(form)
     return render_template(f'{NAME}/register.html', form=form)
@@ -39,7 +78,8 @@ def register():
 
 @bp.route('/logout')
 def logout():
-    return 'logout'
+    session.pop('user_id', None)
+    return redirect(url_for(f'{NAME}.login'))
 
 
 def flash_form_errors(form):
