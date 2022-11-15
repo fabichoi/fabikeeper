1163ed74c85cb083565d6c07f46bedb4022df9fe
fabichoi <fabianus.c@gmail.com>
2022-11-15 20:55:28 +0900

    [API] DB 연결 세번째 시간 커밋

---
+++ fabikeeper/models/user.py
@@ -0,0 +1,8 @@
+from fabikeeper import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(20), unique=True, nullable=False)
+    user_name = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
