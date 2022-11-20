1518f51fa01d97efb1f093eff64a8920131845cc
fabichoi <fabianus.c@gmail.com>
2022-11-20 17:31:41 +0900

    [WEB] html 파일들 템플릿 기능 활용해서 리펙토링 및 라이센스 파일 추가

--- fabikeeper/routes/auth_route.py
+++ fabikeeper/routes/auth_route.py
@@ -1,26 +1,24 @@
-from flask import Blueprint, render_template, redirect, url_for, flash, session, request
+from flask import Blueprint, render_template, redirect, url_for, flash, session, request, g
 from fabikeeper.forms.auth_form import LoginForm, RegisterForm
 from fabikeeper.models.user import User as UserModel
+from fabikeeper import db
 from werkzeug import security
 
 NAME = 'auth'
 bp = Blueprint(NAME, __name__, url_prefix='/auth')
 
-from dataclasses import dataclass
 
-USERS = []
+@bp.before_app_request
+def before_app_request():
+    g.user = None
+    user_id = session.get('user_id')
 
-
-@dataclass
-class User:
-    user_id: str
-    user_name: str
-    password: str
-
-
-USERS.append(User('abcd', 'abcd', security.generate_password_hash('1234')))
-USERS.append(User('admin', 'admin', security.generate_password_hash('1234')))
-USERS.append(User('tester', 'tester', security.generate_password_hash('1234')))
+    if user_id:
+        user = UserModel.find_one_by_user_id(user_id)
+        if user:
+            g.user = user
+        else:
+            session.pop('user_id', None)
 
 
 @bp.route('/')
@@ -34,9 +32,8 @@ def login():
     if form.validate_on_submit():
         user_id = form.data.get('user_id')
         password = form.data.get('password')
-        user = [user for user in USERS if user.user_id == user_id]
+        user = UserModel.find_one_by_user_id(user_id)
         if user:
-            user = user[0]
             if not security.check_password_hash(user.password, password):
                 flash('Password is not valid.')
             else:
@@ -58,18 +55,19 @@ def register():
         user_name = form.data.get('user_name')
         password = form.data.get('password')
         repassword = form.data.get('repassword')
-        user = [user for user in USERS if user.user_id == user_id]
+        user = UserModel.find_one_by_user_id(user_id)
         if user:
             flash('User ID is already exists.')
             return redirect(request.path)
         else:
-            USERS.append(
-                User(
+            g.db.add(
+                UserModel(
                     user_id=user_id,
                     user_name=user_name,
                     password=security.generate_password_hash(password)
                 )
             )
+            g.db.commit()
             session['user_id'] = user_id
             return redirect(url_for('base.index'))
         return f'{user_id}, {user_name}, {password}, {repassword}'
