a97899fd57c46c48e2bd101d75b9f33dd1f4bfa1
fabichoi <fabianus.c@gmail.com>
2022-11-30 22:44:17 +0900

    [-] 모델 마이그레이션 및 관련 로직 수정

--- fabikeeper/models/user.py
+++ fabikeeper/models/user.py
@@ -1,4 +1,5 @@
 from fabikeeper import db
+from sqlalchemy import func
 
 
 class User(db.Model):
@@ -6,6 +7,7 @@ class User(db.Model):
     user_id = db.Column(db.String(20), unique=True, nullable=False)
     user_name = db.Column(db.String(20), nullable=False)
     password = db.Column(db.String(100), nullable=False)
+    created_at = db.Column(db.DateTime(), server_default=func.now())
 
     @classmethod
     def find_one_by_user_id(cls, user_id):
