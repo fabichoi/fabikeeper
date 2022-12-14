ae69859f43468348cbe2fb33914532c86fdf185a
fabichoi <fabianus.c@gmail.com>
2022-12-14 20:51:40 +0900

    [API] 세션 관련 적용 첫번째 시간

--- fabikeeper/routes/base_route.py
+++ fabikeeper/routes/base_route.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, g, redirect, url_for
 
 NAME = 'base'
 
@@ -7,4 +7,6 @@ bp = Blueprint(NAME, __name__)
 
 @bp.route('/')
 def index():
+    if not g.user:
+        return redirect(url_for('auth.login'))
     return render_template('index.html')
