f539872d4fc157f97054307a0d5ebd330ad9914b
fabichoi <fabianus.c@gmail.com>
2023-01-06 01:36:52 +0900

    [API] 메모 이미지 추가 API 작성

--- fabikeeper/apis/memo.py
+++ fabikeeper/apis/memo.py
@@ -1,4 +1,8 @@
-from flask import g
+import os.path
+
+from flask import g, current_app
+from werkzeug.datastructures import FileStorage
+from werkzeug.utils import secure_filename
 
 from fabikeeper.models.user import User as UserModel
 from fabikeeper.models.memo import Memo as MemoModel
@@ -14,6 +18,7 @@ memo = ns.model('Memo', {
     'user_id': fields.Integer(required=True, description='유저 고유 아이디'),
     'title': fields.String(required=True, description='메모 제목'),
     'content': fields.String(required=True, description='메모 내용'),
+    'linked_image': fields.String(required=False, description='메모 이미지'),
     'created_at': fields.DateTime(description='메모 작성일'),
     'updated_at': fields.DateTime(description='메모 변경일'),
 })
@@ -21,6 +26,7 @@ memo = ns.model('Memo', {
 parser = reqparse.RequestParser()
 parser.add_argument('title', required=True, help='메모 제목')
 parser.add_argument('content', required=True, help='메모 내용')
+parser.add_argument('linked_image', location='files', required=False, type=FileStorage, help='메모 이미지 경로')
 
 put_parser = parser.copy()
 put_parser.replace_argument('title', required=False, help='메모 제목')
@@ -29,6 +35,46 @@ put_parser.replace_argument('content', required=False, help='메모 내용')
 get_parser = reqparse.RequestParser()
 get_parser.add_argument('page', required=False, help="메모 페이지 번호")
 get_parser.add_argument('needle', required=False, location='args', help="메모 검색어")
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {
+        'jpg', 'jpeg', 'png', 'gif'
+    }
+
+
+def randomword(length):
+    import random, string
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
+
+
+def save_file(file):
+    if file.filename == '':
+        ns.abort(400)
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        relative_path = os.path.join(
+            current_app.static_url_path[1:],
+            current_app.config['USER_STATIC_BASE_DIR'],
+            g.user.user_id,
+            'memos',
+            randomword(5),
+            filename
+        )
+        upload_path = os.path.join(
+            current_app.root_path,
+            relative_path
+        )
+        os.makedirs(
+            os.path.dirname(upload_path),
+            exist_ok=True
+        )
+        file.save(upload_path)
+        return relative_path, upload_path
+    else:
+        ns.abort(400)
 
 
 @ns.route('')
@@ -53,7 +99,7 @@ class MemoList(Resource):
         if needle:
             needle = f'%%{needle}%%'
             base_query.filter(
-                MemoModel.title.ilike(needle)|MemoModel.content.ilike(needle)
+                MemoModel.title.ilike(needle) | MemoModel.content.ilike(needle)
             )
 
         pages = base_query.order_by(
@@ -75,6 +121,10 @@ class MemoList(Resource):
             content=args['content'],
             user_id=g.user.id,
         )
+        file = args['linked_image']
+        if file:
+            relative_path, _ = save_file(file)
+            memo.linked_image = relative_path
         g.db.add(memo)
         g.db.commit()
         return memo, 201
