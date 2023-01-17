cb5dc2db38680512578e0e00d02fa85ad988546a
fabichoi <fabianus.c@gmail.com>
2023-01-17 21:05:13 +0900

    [API] 메모 이미지 조회/추가/삭제 FE 구현

--- fabikeeper/static/js/ajax.js
+++ fabikeeper/static/js/ajax.js
@@ -290,6 +290,10 @@ const MEMO = (function () {
                 $modalContent.val(r.content);
                 $modalClose.attr('data-id', r.id);
                 $modalContent.trigger('keyup');
+                if (r.linked_image) {
+                    const ihtml = '<img src="' + r.linked_image + '" />';
+                    $modalMedia.html(ihtml);
+                }
             }
         })
     };
@@ -332,6 +336,10 @@ const MEMO = (function () {
                 success: function (r) {
                     $title.html(r.title);
                     $content.html(r.content);
+                    if (r.linked_image) {
+                        const ihtml = '<img src="' + r.linked_image + '" />'
+                        $media.html(ihtml);
+                    }
                 },
                 error: function (e) {
                     alert(e.responseText);
@@ -374,6 +382,19 @@ const MEMO = (function () {
             // 2) 메모 아이템에 메모삭제
             // 3) 에러시 얼럿노출
             // 4) 완료시 모달 리셋: resetModalFields(true);
+            $.ajax({
+                url: '/api/memos/' + id + '/image',
+                type: 'delete',
+                success: function (r) {
+                    $media.html('');
+                },
+                error: function (e) {
+                    alert(e.responseText);
+                },
+                complete: function () {
+                    resetModalFields(true);
+                }
+            })
         }
     }
 
